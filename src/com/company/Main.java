1294baea430a582a8040af66e80fe6d0dae0874f
romakv4@gmail.com <romakv4@gmail.com>
2017-01-31 18:24:42 +0600

    No OOP, template bot for telegram

---
+++ src/com/company/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class Main extends TelegramLongPollingBot {
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Main());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "WeatherTlgrm_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "305385180:AAGx5qFwRSXTHV_t-IK-TCl801V3AUnV4ps";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            if (message.getText().equals("/help"))
+                sendMsg(message, "Привет, я робот");
+            else
+                sendMsg(message, "Ты амеба? Диалоги мне пропиши)))))))))))))0");
+        }
+    }
+
+    private void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
