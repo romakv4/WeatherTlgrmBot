3ead355f9dae1d1040ea0b0d13a6ee8f40a35581
romakv4@gmail.com <romakv4@gmail.com>
2017-05-09 01:38:59 +0600

    Conditions of keyboard.

---
+++ src/main/java/telegram/weatherBot/BotsKeyboards.java
@@ -0,0 +1,30 @@
+package telegram.weatherBot;
+
+import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Keyboards for my bot.
+ */
+class BotsKeyboards {
+    InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+    void keyboardForSimple() {
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+        List<InlineKeyboardButton> keys = new ArrayList<>();
+        InlineKeyboardButton button = new InlineKeyboardButton();
+        keys.add(button.setText("Подробно").setCallbackData("Подробно"));
+        keyboard.add(keys);
+        markup.setKeyboard(keyboard);
+    }
+    void keyboardForDetail() {
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+        List<InlineKeyboardButton> keys = new ArrayList<>();
+        InlineKeyboardButton button = new InlineKeyboardButton();
+        keys.add(button.setText("Кратко").setCallbackData("Кратко"));
+        keyboard.add(keys);
+        markup.setKeyboard(keyboard);
+    }
+}
--- src/main/java/telegram/weatherBot/WeatherTelegramBot.java
+++ src/main/java/telegram/weatherBot/WeatherTelegramBot.java
@@ -3,6 +3,8 @@ package telegram.weatherBot;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.methods.send.SendSticker;
+import org.telegram.telegrambots.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.api.objects.CallbackQuery;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
@@ -23,7 +25,7 @@ import static telegram.weatherBot.WeatherGetting.getWeather;
 public class WeatherTelegramBot extends TelegramLongPollingBot {
 
     private String city = null;
-    private boolean isDetail = false;
+    private boolean firstMessageIsSend = false;
 
     public static void main(String[] args) {
         init();
@@ -51,60 +53,65 @@ public class WeatherTelegramBot extends TelegramLongPollingBot {
             Message message = update.getMessage();
             if (message != null && message.hasText()) {
                 if (message.getText().equals("/start")) {
-                    sendMessage(message, "Теперь введите название своего города", true);
+                    sendMsg(message, "Теперь введите название своего города", true);
                 } else if (message.getText().equals("/help")) {
-                    sendMessage(message, "Введите название города, чтобы узнать текущую погоду в нем", true);
-                } else if (message.getText() != null && !message.getText().equals("/help") &&
-                        !message.getText().equals("/start") && !message.getText().equals("Подробнее")) {
-                    sendMessage(message, getSimplyTemperature(message.getText()), true);
-                    city = message.getText();
+                    sendMsg(message, "Введите название города, чтобы узнать текущую погоду в нем", true);
                 }
             }
         } else if (update.hasCallbackQuery()) {
             CallbackQuery query = update.getCallbackQuery();
-            AnswerCallbackQuery answer = new AnswerCallbackQuery();
-            answer.setCallbackQueryId(query.getId());
+            AnswerCallbackQuery answerCallbackQuery = new AnswerCallbackQuery();
+            answerCallbackQuery.setCallbackQueryId(query.getId());
             if (query.getData().equals("Подробно")) {
-                isDetail = true;
-                sendMessage(query.getMessage(), "For " + city + "\n" + getWeather(city), false);
+                BotsKeyboards botsKeyboards = new BotsKeyboards();
+                botsKeyboards.keyboardForDetail();
+                String answer = getWeather(city);
+                EditMessageText new_message = new EditMessageText()
+                        .setChatId(update.getCallbackQuery().getMessage().getChatId())
+                        .setMessageId(update.getCallbackQuery().getMessage().getMessageId())
+                        .enableMarkdown(true)
+                        .setReplyMarkup(botsKeyboards.markup)
+                        .setText(answer.replaceAll("\\[|]", ""));
+                try {
+                    editMessageText(new_message);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
             } else if (query.getData().equals("Кратко")) {
-                isDetail = false;
-                sendMessage(query.getMessage(), "For " + city + "\n" + getSimplyTemperature(city), false);
-            }
-            try {
-                answerCallbackQuery(answer);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+                String answer = getSimplyTemperature(city);
+                BotsKeyboards botsKeyboards = new BotsKeyboards();
+                botsKeyboards.keyboardForSimple();
+                EditMessageText new_message = new EditMessageText()
+                        .setChatId(update.getCallbackQuery().getMessage().getChatId())
+                        .setMessageId(update.getCallbackQuery().getMessage().getMessageId())
+                        .enableMarkdown(true)
+                        .setReplyMarkup(botsKeyboards.markup)
+                        .setText(answer.replaceAll("\\[|]", ""));
+                try {
+                    editMessageText(new_message);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
-    private void sendMessage(Message message, String text, boolean isReplay) {
+    private void sendMsg(Message message, String text, boolean isReplay) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(message.getChatId().toString());
         if (isReplay) {
             sendMessage.setReplyToMessageId(message.getMessageId());
         }
-        if (!isReplay) {
-            sendMessage.setReplyToMessageId(message.getMessageId());
-        }
-        sendMessage.setText(text);
         InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
         List<InlineKeyboardButton> keys = new ArrayList<>();
         InlineKeyboardButton button = new InlineKeyboardButton();
-        if (isDetail == false) {
-            button.setText("Подробно");
-            button.setCallbackData("Подробно");
-        } else if (isDetail == true) {
-            button.setText("Кратко");
-            button.setCallbackData("Кратко");
-        }
-        keys.add(button);
+        keys.add(button.setText("Подробно").setCallbackData("Подробно"));
         keyboard.add(keys);
         markup.setKeyboard(keyboard);
         sendMessage.setReplyMarkup(markup);
+        sendMessage.setText(text);
         try {
             sendMessage(sendMessage);
         } catch (TelegramApiException e) {
